e5eddb9758387078eb82b997bab02ef4c077baa9
Jacob <jhillebrand25@gmail.com>
2020-12-02 20:51:48 -0600

    Finished Day 1

---
+++ day_1/main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	string input_file = "input.txt";
+
+	fstream file_handler;
+	file_handler.open(input_file, ios::in);
+	
+	if (file_handler.is_open()) {
+
+		//Read file into vector
+		vector<string> input_vector;
+		string current_string;
+		while (getline(file_handler, current_string)) {
+			input_vector.push_back(current_string);
+		}
+
+		for (int i = 0; i < input_vector.size() - 1; i++){
+			//DEBUG
+			cout << "i = " << i << endl;
+			int i_value = stoi(input_vector[i]);
+			for (int k = i + 1; k < input_vector.size() - 1; k++) {
+				//DEBUG
+				cout << "k = " << k << endl;
+				int k_value = stoi(input_vector[k]);
+				if (i_value + k_value == 2020) {
+					cout << "Answer: " << i_value*k_value << endl;
+					return 0;
+				}
+			}
+		}
+
+	}
+	else {
+		cout << "File open failed. Sorry! :)" << endl;
+	}
+
+	file_handler.close();
+
+	return 0;
+
+}
