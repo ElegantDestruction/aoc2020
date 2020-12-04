f7edc8aa549bf0577a907d9dd3039ae78f714644
Jacob <jhillebrand25@gmail.com>
2020-12-03 19:41:10 -0600

    Debugged day 2 part 1

--- day_2/part_1/toboggan.py
+++ day_2/part_1/toboggan.py
@@ -12,20 +12,23 @@ passwd_count = 0
 # Search
 for current_line in full_input:
     #1 strip on colon
-    full_string = current_line.split(' ')
+    full_string = current_line.split(': ')
+    for line in full_string:
+        print(line)
+    print()
     #2 strip on space
     passwd_info_full = full_string[0].split(' ')
+    for line in passwd_info_full:
+        print(line)
+    print()
     #3 strip on hypen
     passwd_length = passwd_info_full[0].split('-')
+    for line in passwd_length:
+        print(line)
+    print()
     #5 search password for end of #2 strip
     startIndex = 0
-    occurrance_count = 0
-    for i in range(len(full_string[1])):
-        k = full_string[1].find('test', startIndex)
-        if(k != -1):
-            startIndex = k+1
-            occurrance_count += 1
-            k = 0
+    occurrance_count = full_string[1].count(passwd_info_full[1])
 
     
     #6 if password occurred between numbers of 3rd strip, increment count
